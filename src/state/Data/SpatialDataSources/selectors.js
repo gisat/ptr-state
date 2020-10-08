928e80d3ef920c9735a9fe247003d868b503e56a
vlach1989 <vlach1989@gmail.com>
2020-10-08 07:00:39 +0200

    SpatialDataSources selectors - get from object

--- src/state/Data/SpatialDataSources/selectors.js
+++ src/state/Data/SpatialDataSources/selectors.js
@@ -1,25 +1,24 @@
 import _ from 'lodash';
 import common from '../../_common/selectors';
-import {makeSelector} from '@taskworld.com/rereselect';
 import {createSelector as createRecomputeSelector, createObserver as createRecomputeObserver} from '@jvitela/recompute';
 
 import SpatialRelationsSelectors from "../SpatialRelations/selectors";
 
 const getSubstate = state => state.data.spatialDataSources;
-const getAll = common.getAll(getSubstate);
+const getAllAsObject = common.getAllAsObject(getSubstate);
 
-const getAllObserver = createRecomputeObserver(getAll);
+const getAllAsObjectObserver = createRecomputeObserver(getAllAsObject);
 
 const getFiltered = createRecomputeSelector(filter => {
 	console.log("SpatialDataSources/selectors#getFiltered");
 
 	const dataSourceKeys = SpatialRelationsSelectors.getFilteredDataSourceKeys(filter);
-	const dataSources = getAllObserver();
+	const dataSources = getAllAsObjectObserver();
 
-	if (dataSourceKeys?.length && dataSources?.length) {
+	if (dataSourceKeys?.length && dataSources) {
 		let filteredDataSources = [];
 		_.forEach(dataSourceKeys, key => {
-			const dataSource = _.find(dataSources, {key});
+			const dataSource = dataSources[key];
 			if (dataSource) {
 				filteredDataSources.push(dataSource.data);
 			}
