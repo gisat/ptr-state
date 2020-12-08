996cc9002194e25733fa758a92ac32d49d1b80ef
vlach1989 <vlach1989@gmail.com>
2020-12-08 21:32:02 +0100

    getMissingFiles: Check filter

--- src/state/Data/helpers.js
+++ src/state/Data/helpers.js
@@ -23,7 +23,7 @@ export const tileAsString = (tile) => {
  *  @param {number} filter.level
  */
 export const getMissingTiles = (index, filter) => {
-    if(index && index.index) {
+    if(filter && index && index.index) {
         if(index?.index[filter.level] && filter && filter.tiles) {
             const loadedTilesInIndex = Object.entries(index.index[filter.level]).reduce((acc, tile) => {
                 const tileKey = tile[0];
