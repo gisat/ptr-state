acff0870049c033ffb0502452e9b26e7d8632c46
vlach1989 <vlach1989@gmail.com>
2021-01-17 12:13:06 +0100

    SpatialData selectors: move some selectors from common

--- src/state/Data/SpatialData/selectors.js
+++ src/state/Data/SpatialData/selectors.js
@@ -1,10 +1,21 @@
 import common from '../../_common/selectors';
 import {createSelector as createRecomputeSelector, createObserver as createRecomputeObserver} from '@jvitela/recompute';
+import commonHelpers from '../../_common/helpers';
 
 const getSubstate = (state) => state.data.spatialData;
 
 const getIndex = common.getIndex(getSubstate);
-const getIndex_recompute = common.getIndex_recompute(getSubstate);
+
+const getIndexesObserver = createRecomputeObserver((state, getSubstate) => common.getIndexes(getSubstate)(state));
+
+const getIndex_recompute = createRecomputeSelector((filter, order) => {
+	const indexes = getIndexesObserver(getSubstate);
+	if (indexes) {
+		return commonHelpers.getIndex(indexes, filter, order);
+	} else {
+		return null;
+	}
+});
 
 const getByDataSourceKeyObserver = createRecomputeObserver((state, key) => {
 	return getSubstate(state)?.byDataSourceKey?.[key] || null;
