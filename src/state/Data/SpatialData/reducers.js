606a831ef939728e76ef56d3b0b660cf6ab27271
vlach1989 <vlach1989@gmail.com>
2021-01-18 10:50:00 +0100

    Spatial data reducers: comments, namings, todo

--- src/state/Data/SpatialData/reducers.js
+++ src/state/Data/SpatialData/reducers.js
@@ -12,20 +12,32 @@ const getEmptyFeature = () => {
     }
 }
 
+/**
+ * Add spatial data
+ * @param state {Object}
+ * @param action {Object}
+ * @param action.key {String} Data source key
+ * @param action.level {number} Zoom level
+ * @param action.data {Object} Features as object
+ * @return {Object}
+ */
+// TODO what about features adding without level?
 const add = (state, action) => {
     const level = action.level;
     const key = action.key;
+	const featuresAsObject = action.data;
+	const dataFeaturesKeys = Object.keys(featuresAsObject);
+
     let stateDataByDataSourceKey = state.byDataSourceKey[action.key] || {};
-    const dataFeatures = action.data;
-    const dataFeaturesKeys = Object.keys(dataFeatures);
+
     dataFeaturesKeys.forEach((featureKey) => {
         if (stateDataByDataSourceKey.hasOwnProperty(featureKey)) {
             //add just level geometry to existing feature
-            stateDataByDataSourceKey[featureKey].geometries[level] = dataFeatures[featureKey]
+            stateDataByDataSourceKey[featureKey].geometries[level] = featuresAsObject[featureKey]
         } else {
             //create new feature with geometry and add to state
             const newFeature = getEmptyFeature();
-            newFeature.geometries[level] = dataFeatures[featureKey]
+            newFeature.geometries[level] = featuresAsObject[featureKey]
             stateDataByDataSourceKey = {...stateDataByDataSourceKey, [featureKey]: {...newFeature}};
         }
     })
