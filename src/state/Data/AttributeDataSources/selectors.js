2ff4d50f46b98711098f3d4db17e9b673d29aede
vlach1989 <vlach1989@gmail.com>
2021-01-18 08:17:02 +0100

    AttributeDataSources selectors: comments & better naming

--- src/state/Data/AttributeDataSources/selectors.js
+++ src/state/Data/AttributeDataSources/selectors.js
@@ -3,20 +3,33 @@ import common from "../../_common/selectors";
 
 const getSubstate = state => state.data.attributeDataSources;
 
+const getIndex = common.getIndex(getSubstate);
 const getIndex_recompute = common.getIndex_recompute(getSubstate);
 
+/**
+ * It returns data source model for given key, if exists
+ * @param key {string} data source key
+ * @return {Object} attribute data source
+ */
 const getByKeyObserver = createRecomputeObserver((state, key) => {
 	return getSubstate(state)?.byKey?.[key] || null;
 });
 
-
-const getIndex = common.getIndex(getSubstate);
-
+/**
+ * It returns data source models for given keys
+ * @param keys {Array} data source keys
+ * @return {Array} A collection of data sources
+ */
 const getByKeys = createRecomputeSelector(keys => {
 	return keys.map(key => getByKeyObserver(key));
 });
 
-const getFiltered = createRecomputeSelector(filter => {
+/**
+ * It returns a collection of indexed data sources for given filter
+ * @param filter {Object}
+ * @return {Array}
+ */
+const getIndexed = createRecomputeSelector(filter => {
 	const index = getIndex_recompute(filter, null);
 	if (index?.index) {
 		let keys = Object.values(index.index);
@@ -31,6 +44,6 @@ const getFiltered = createRecomputeSelector(filter => {
 });
 
 export default {
-	getFiltered,
+	getIndexed,
 	getIndex,
 };
