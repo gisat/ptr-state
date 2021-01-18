08c898f1f73c7ffb2032bfee0eadfd4310412dcd
vlach1989 <vlach1989@gmail.com>
2021-01-18 07:19:40 +0100

    SpatialData selectors: comments

--- src/state/Data/SpatialData/selectors.js
+++ src/state/Data/SpatialData/selectors.js
@@ -8,6 +8,21 @@ const getIndex = common.getIndex(getSubstate);
 
 const getIndexesObserver = createRecomputeObserver((state, getSubstate) => common.getIndexes(getSubstate)(state));
 
+/**
+ * It returns all data for given datasource key
+ * @param key {string} data source key
+ * @returns {Object} Features as object (by feature key)
+ */
+const getByDataSourceKeyObserver = createRecomputeObserver((state, key) => {
+	return getSubstate(state)?.byDataSourceKey?.[key] || null;
+});
+
+/**
+ * It returns whole index for given filter & order
+ * @param {Object} filter
+ * @param {Array} order
+ * @return {Object} index
+ */
 const getIndex_recompute = createRecomputeSelector((filter, order) => {
 	const indexes = getIndexesObserver(getSubstate);
 	if (indexes) {
@@ -17,10 +32,13 @@ const getIndex_recompute = createRecomputeSelector((filter, order) => {
 	}
 });
 
-const getByDataSourceKeyObserver = createRecomputeObserver((state, key) => {
-	return getSubstate(state)?.byDataSourceKey?.[key] || null;
-});
-
+/**
+ * @param {Object} filter
+ * @param {number} level
+ * @param {string} tile
+ * @param {string} dataSourceKey
+ * @return {Array} indexed feature keys
+ */
 const getIndexedFeatureKeys = createRecomputeSelector((filter, level, tile, dataSourceKey) => {
 	const index = getIndex_recompute(filter, null);
 	if (index?.index) {
