dfd33146be11f68bd4d3729fb19c4e6de2d1246a
vlach1989 <vlach1989@gmail.com>
2021-01-18 10:59:59 +0100

    Attribute data reducers: comments

--- src/state/Data/AttributeData/reducers.js
+++ src/state/Data/AttributeData/reducers.js
@@ -6,13 +6,30 @@ const INITIAL_STATE = {
     byDataSourceKey: {},
 };
 
+/**
+ * Add attribute data
+ * @param state {Object}
+ * @param action {Object}
+ * @param action.key {string} attribute data source key
+ * @param action.data {Object} feature key - attribute value pairs
+ * @return {Object}
+ */
 const add = (state, action) => {
     return {...state, byDataSourceKey: {...state.byDataSourceKey, [action.key]: {...action.data}}}
 }
 
+/**
+ * update attribute data for given data source
+ * @param state {Object}
+ * @param action {Object}
+ * @param action.key {string} attribute data source key
+ * @param action.data {Object} feature key - attribute value pairs
+ * @return {Object}
+ */
 const update = (state, action) => {
     return {...state, byDataSourceKey: {...state.byDataSourceKey, [action.key]: {...state.byDataSourceKey[action.key], ...action.data}}}
 }
+
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case ActionTypes.DATA.ATTRIBUTE_DATA.ADD:
