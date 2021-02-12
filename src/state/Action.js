f21a4def734d8c0b8c9ba2b4ed41c858ef2a0a06
vlach1989 <vlach1989@gmail.com>
2021-02-12 18:22:30 +0100

    remove deprecated and obsolete code

--- src/state/Action.js
+++ src/state/Action.js
@@ -1,20 +1,11 @@
-import _deprecatedMaps from './_deprecatedMaps/actions';
-import _deprecatedSelections from './_deprecatedSelections/actions';
-
 import App from './App/actions';
 import Areas from './Areas/actions';
 import AreaRelations from './AreaRelations/actions';
 import Attributes from './Attributes/actions';
-import AttributeRelations from './AttributeRelations/actions';
-import AttributeDataSources from './AttributeDataSources/actions';
-import AttributeData from './AttributeData/actions';
 import AttributeSets from './AttributeSets/actions';
-import AttributeStatistics from './AttributeStatistics/actions';
 import Cases from './Cases/actions';
-import Charts from './Charts/actions';
 import Components from './Components/actions';
 import Data from './Data/actions';
-import LayerPeriods from "./LayerPeriods/actions";
 import LayerTemplates from './LayerTemplates/actions';
 import LayerTrees from './LayerTrees/actions';
 import Maps from './Maps/actions';
@@ -24,10 +15,6 @@ import Scenarios from './Scenarios/actions';
 import Scopes from './Scopes/actions';
 import Screens from './Screens/actions';
 import Selections from './Selections/actions';
-import Snapshots from './Snapshots/actions';
-import SpatialData from './SpatialData/actions';
-import SpatialDataSources from './SpatialDataSources/actions';
-import SpatialRelations from './SpatialRelations/actions';
 import Styles from './Styles/actions';
 import Tags from './Tags/actions';
 import Users from './Users/actions';
@@ -35,35 +22,23 @@ import Views from './Views/actions';
 import Windows from './Windows/actions';
 
 export default {
-	_deprecatedSelections: _deprecatedSelections,
 	app: App,
 	areas: Areas,
 	areaRelations: AreaRelations,
 	attributes: Attributes,
-	attributeData: AttributeData,
-	attributeDataSources: AttributeDataSources,
-	attributeRelations: AttributeRelations,
-	attributeStatistics: AttributeStatistics,
 	attributeSets: AttributeSets,
 	cases: Cases,
-	charts: Charts,
 	components: Components,
 	data: Data,
-	layerPeriods: LayerPeriods,
 	layerTemplates: LayerTemplates,
 	layerTrees: LayerTrees,
     maps: Maps,
-	_deprecatedMaps: _deprecatedMaps,
 	periods: Periods,
 	places: Places,
 	scenarios: Scenarios,
 	scopes: Scopes,
 	screens: Screens,
 	selections: Selections,
-	snapshots: Snapshots,
-	spatialData: SpatialData,
-	spatialDataSources: SpatialDataSources,
-	spatialRelations: SpatialRelations,
 	styles: Styles,
 	tags: Tags,
 	users: Users,
