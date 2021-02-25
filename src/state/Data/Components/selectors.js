a0e905493ab8d0155e660389ade9355b292fcc77
vlach1989 <vlach1989@gmail.com>
2021-02-25 10:34:03 +0100

    Data/Components - getData - start & length

--- src/state/Data/Components/selectors.js
+++ src/state/Data/Components/selectors.js
@@ -20,86 +20,101 @@ const getComponentStateByKeyObserver = createRecomputeObserver(
 const getData = createRecomputeSelector(componentKey => {
 	const componentState = getComponentStateByKeyObserver(componentKey);
 
-	// TODO cached selector for data of only relevant data sources needed!!!
-	const data = attributeDataSelectors.getAllAsObject_recompute();
-	const attributeKeys = componentState?.attributeKeys;
+	if (componentState) {
+		// TODO cached selector for data of only relevant data sources needed!!!
+		const data = attributeDataSelectors.getAllAsObject_recompute();
+		const attributeKeys = componentState?.attributeKeys;
 
-	if (!_.isEmpty(data) && attributeKeys?.length) {
-		// Get common relations filter
-		let commonRelationsFilter = common.getCommmonDataRelationsFilterFromComponentState_recompute(
-			componentState
-		);
-
-		// Create final relations filter
-		const relationsFilter = {
-			...commonRelationsFilter,
-			attributeKeys,
-		};
-
-		// Get relations
-		const attributeRelations = attributeRelationsSelectors.getIndexed(
-			relationsFilter
-		);
-
-		if (attributeRelations?.length) {
-			// Get from relations, which data source is associated with which attribute
-			let attributeDsKeyAttributeKeyPairs = {};
-			attributeRelations.forEach(relation => {
-				attributeDsKeyAttributeKeyPairs[relation.data.attributeDataSourceKey] =
-					relation.data.attributeKey;
-			});
-
-			// Find data index
-			// TODO more sophisticated index with attributeFilter & attributeOrder
-			const attributeDataIndex = attributeDataSelectors.getIndex_recompute(
-				relationsFilter,
-				null
+		if (!_.isEmpty(data) && attributeKeys?.length) {
+			// Get common relations filter
+			let commonRelationsFilter = common.getCommmonDataRelationsFilterFromComponentState_recompute(
+				componentState
 			);
 
-			// Get indexed features
-			const indexedFeatureKeys = attributeDataIndex?.index;
+			// Create final relations filter
+			const relationsFilter = {
+				...commonRelationsFilter,
+				attributeKeys,
+			};
 
-			if (indexedFeatureKeys) {
-				let finalFeaturesAsObject = [];
+			// Get relations
+			const attributeRelations = attributeRelationsSelectors.getIndexed(
+				relationsFilter
+			);
 
-				// Loop through indexed features
-				_.forIn(indexedFeatureKeys, (featureKey, index) => {
-					// We don't know which feature is in which attribute DS
-					// also there could be more attributes for the feature
-					_.forIn(
-						attributeDsKeyAttributeKeyPairs,
-						(attributeKey, attributeDsKey) => {
-							let value = data[attributeDsKey]?.[featureKey];
+			if (attributeRelations?.length) {
+				// Get from relations, which data source is associated with which attribute
+				let attributeDsKeyAttributeKeyPairs = {};
+				attributeRelations.forEach(relation => {
+					attributeDsKeyAttributeKeyPairs[
+						relation.data.attributeDataSourceKey
+					] = relation.data.attributeKey;
+				});
 
-							if (value !== undefined) {
-								// existing feature
-								if (finalFeaturesAsObject[index]) {
-									finalFeaturesAsObject[index].data[attributeKey] = value;
-								}
+				// Find data index
+				// TODO more sophisticated index with attributeFilter & attributeOrder
+				const attributeDataIndex = attributeDataSelectors.getIndex_recompute(
+					relationsFilter,
+					null
+				);
 
-								// new feature
-								else {
-									// TODO temporary fix for buggy BE values datatype
-									value = isNaN(value)
-										? value
-										: _.isNumber(value)
-										? value
-										: Number(value);
+				// Get indexed features
+				const indexedFeatureKeysAsObject = attributeDataIndex?.index;
 
-									// TODO format?
-									finalFeaturesAsObject[index] = {
-										key: featureKey,
-										data: {
-											[attributeKey]: value,
-										},
-									};
+				if (indexedFeatureKeysAsObject) {
+					let indexedFeatureKeys = Object.values(indexedFeatureKeysAsObject);
+
+					const {start, length} = componentState;
+					if ((start || start === 0) && length) {
+						indexedFeatureKeys = indexedFeatureKeys.slice(
+							start,
+							start + length
+						);
+					}
+
+					let finalFeaturesAsObject = [];
+
+					// Loop through indexed features
+					_.forEach(indexedFeatureKeys, (featureKey, index) => {
+						// We don't know which feature is in which attribute DS
+						// also there could be more attributes for the feature
+						_.forIn(
+							attributeDsKeyAttributeKeyPairs,
+							(attributeKey, attributeDsKey) => {
+								let value = data[attributeDsKey]?.[featureKey];
+
+								if (value !== undefined) {
+									// existing feature
+									if (finalFeaturesAsObject[index]) {
+										finalFeaturesAsObject[index].data[attributeKey] = value;
+									}
+
+									// new feature
+									else {
+										// TODO temporary fix for buggy BE values datatype
+										value = isNaN(value)
+											? value
+											: _.isNumber(value)
+											? value
+											: Number(value);
+
+										// TODO format?
+										finalFeaturesAsObject[index] = {
+											key: featureKey,
+											data: {
+												[attributeKey]: value,
+											},
+										};
+									}
 								}
 							}
-						}
-					);
-				});
-				//Remove empty items from array
-				return finalFeaturesAsObject.filter(i => i);
+						);
+					});
+					//Remove empty items from array
+					return finalFeaturesAsObject.filter(i => i);
+				} else {
+					return null;
+				}
 			} else {
 				return null;
 			}
