46b91ac783bb7ea224afaad8b27a9bd98c93d541
vlach1989 <vlach1989@gmail.com>
2021-03-01 15:18:35 +0100

    add missing action types & reducers

--- src/state/Attributes/reducers.js
+++ src/state/Attributes/reducers.js
@@ -47,6 +47,8 @@ export default (state = INITIAL_STATE, action) => {
 			return common.markDeleted(state, action);
 		case ActionTypes.ATTRIBUTES.SET_ACTIVE_KEY:
 			return common.setActive(state, action);
+		case ActionTypes.ATTRIBUTES.SET_ACTIVE_KEYS:
+			return common.setActiveMultiple(state, action);
 		case ActionTypes.ATTRIBUTES.USE.INDEXED.CLEAR:
 			return common.useIndexedClear(state, action);
 		case ActionTypes.ATTRIBUTES.USE.INDEXED.REGISTER:
