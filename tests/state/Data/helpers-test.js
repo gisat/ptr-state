4d3455d124847599f69d1f202a0e9c248656523d
Vojtěch Dubrovský <v.dubr@hotmail.com>
2021-03-09 15:15:45 +0100

    Fix tests

--- tests/state/Data/helpers-test.js
+++ tests/state/Data/helpers-test.js
@@ -4,22 +4,14 @@ import {getPageSize} from '../../../src/state/Data/helpers';
 describe('state/Data/helpers', function () {
 	describe('getPageSize', function () {
 		it('It return pageSize from state', function () {
-			const mockState = {
-				app: {
-					localConfiguration: {
-						requestPageSize: 99,
-					},
-				},
+			const localConfiguration = {
+				requestPageSize: 99,
 			};
-			assert.deepStrictEqual(getPageSize(mockState), 99);
+			assert.deepStrictEqual(getPageSize(localConfiguration), 99);
 		});
 		it('It return pageSize from ptr-core', function () {
-			const mockState = {
-				app: {
-					localConfiguration: {},
-				},
-			};
-			assert.deepStrictEqual(getPageSize(mockState), 100);
+			const localConfiguration = {};
+			assert.deepStrictEqual(getPageSize(localConfiguration), 100);
 		});
 	});
 });
