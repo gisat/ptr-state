7293ad43b55564d9f29aefcbe8792a972a31c882
Vojtěch Dubrovský <v.dubr@hotmail.com>
2021-03-25 08:59:46 +0100

    Add data/spatialResources and data/spatialRelations tests

---
+++ tests/state/Data/SpatialDataSources/actions/add-test.js
@@ -0,0 +1,25 @@
+import {assert} from 'chai';
+import actions from '../../../../../src/state/Data/AttributeDataSources/actions';
+import getStoreSet from '../../../_common/helpers/store';
+describe('state/Data/SpatialDataSources/actions/add', function () {
+	it('Dispatch add action', function () {
+		const storeHelpers = getStoreSet();
+		const getState = () => {
+			return () => ({});
+		};
+		const dispatch = storeHelpers.getDispatch(getState);
+
+		const filter = {appKey: 'testKey'};
+		const data = [{11: 'Prague'}];
+		dispatch(actions.add(data, filter));
+		return storeHelpers.runFunctionActions({dispatch, getState}).then(() => {
+			assert.deepEqual(storeHelpers.getDispatchedActions(), [
+				{
+					type: 'DATA.ATTRIBUTE_DATA_SOURCES.ADD',
+					filter,
+					data,
+				},
+			]);
+		});
+	});
+});
