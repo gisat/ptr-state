4a1efb45e2d49f825e09686bd809ea89a155324f
Vojtěch Dubrovský <v.dubr@hotmail.com>
2021-04-22 08:18:03 +0200

    Better loading initialization

--- src/state/Data/AttributeRelations/actions.js
+++ src/state/Data/AttributeRelations/actions.js
@@ -59,7 +59,7 @@ function addLoadingIndex(pagination, filter, order) {
 	// Action "common.addIndex" needs array of data objects with key to create new index.
 	// "data" is a Array of the minimal data for construct index in common actoin.
 	// Use key = true as a loading identificator
-	const data = [...Array(pagination.limit)].map(() => ({key: true}));
+	const data = new Array(pagination.limit).fill({key: true});
 
 	// filter, order, data, start, count, changedOn
 	return addIndexAction(
