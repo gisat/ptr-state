1ed7478b6dcf10b94c2836ea3091bdbec14589b4
Vojtěch Dubrovský <v.dubr@hotmail.com>
2021-05-21 12:54:12 +0200

    Add tests for actionGeneralError, action, add, addIndex, create and creator

---
+++ tests/state/_common/actions/add-test.js
@@ -0,0 +1,48 @@
+import {assert} from 'chai';
+import commonActions from '../../../../src/state/_common/actions';
+import testBatchRunner from '../../helpers';
+import {commonActionTypesObj as actionTypes} from '../../../constants';
+
+const tests = [
+	{
+		name: 'Dispatch add and wrap data by array',
+		action: (actions, actionTypes, options) => {
+			let action;
+			// for common testing
+			if (actionTypes && options) {
+				action = actions.add(actionTypes);
+			} else {
+				action = actions.useIndexed;
+			}
+
+			return action('data', 'filter');
+		},
+		dispatchedActions: [{type: 'ADD', data: ['data'], filter: 'filter'}],
+	},
+	{
+		name: 'Dispatch add',
+		action: (actions, actionTypes, options) => {
+			let action;
+			// for common testing
+			if (actionTypes && options) {
+				action = actions.add(actionTypes);
+			} else {
+				action = actions.useIndexed;
+			}
+
+			return action([{data: 'key1'}], 'filter');
+		},
+		dispatchedActions: [
+			{type: 'ADD', data: [{data: 'key1'}], filter: 'filter'},
+		],
+	},
+];
+
+const dataType = 'testStore';
+const categoryPath = 'metadata';
+describe(
+	'add',
+	testBatchRunner(dataType, categoryPath, tests, commonActions, actionTypes)
+);
+
+export default tests;
---
+++ tests/state/_common/actions/create-test.js
@@ -0,0 +1,100 @@
+import {assert} from 'chai';
+import slash from 'slash';
+import commonActions from '../../../../src/state/_common/actions';
+import testBatchRunner from '../../helpers';
+import {commonActionTypesObj as actionTypes} from '../../../constants';
+
+const tests = [
+	{
+		name: 'Missing keys',
+		action: (actions, actionTypes, options) => {
+			return (dispatch, getState) => {
+				let action;
+				// for common testing
+				if (actionTypes && options) {
+					action = actions.create(
+						options.getSubstate,
+						options.dataType,
+						actionTypes,
+						options.categoryPath
+					);
+				} else {
+					action = actions.create;
+				}
+
+				return dispatch(action('k1', 'ak'));
+			};
+		},
+		getState: dataType => () => ({
+			app: {
+				localConfiguration: {
+					apiBackendProtocol: 'http',
+					apiBackendHost: 'localhost',
+					apiBackendPath: '',
+				},
+			},
+			[dataType]: {
+				indexes: [{filter: {applicationKey: 'ak'}}],
+			},
+		}),
+		setFetch: (dataType, categoryPath) => (url, options) => {
+			assert.strictEqual(`http://localhost/rest/${categoryPath}`, slash(url));
+			assert.deepStrictEqual(options, {
+				body: JSON.stringify({
+					data: {[dataType]: [{key: 'k1', data: {applicationKey: 'ak'}}]},
+				}),
+				credentials: 'include',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				method: 'POST',
+			});
+
+			return Promise.resolve({
+				ok: true,
+				json: function () {
+					return Promise.resolve(JSON.parse(options.body));
+				},
+				headers: {
+					get: function (name) {
+						return {'Content-type': 'application/json'}[name];
+					},
+				},
+				data: options.body,
+			});
+		},
+		dispatchedActions: options => {
+			return [
+				{
+					type: 'ADD',
+					filter: undefined,
+					data: [
+						{
+							key: 'k1',
+							data: {
+								applicationKey: 'ak',
+							},
+						},
+					],
+				},
+				{
+					type: 'INDEX.CLEAR_INDEX',
+					filter: {
+						applicationKey: 'ak',
+					},
+					order: undefined,
+				},
+			];
+		},
+	},
+];
+
+const dataType = 'testStore';
+const categoryPath = 'metadata';
+describe(
+	'create',
+	testBatchRunner(dataType, categoryPath, tests, commonActions, actionTypes)
+);
+
+export default tests;
