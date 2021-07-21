aae67b0756ffc518a4f30cc1d3a71f2b66121790
Vojtěch Dubrovský <v.dubr@hotmail.com>
2021-07-21 09:34:01 +0200

    Add getIndexed selector

--- src/state/Areas/AreaTreeLevels/selectors.js
+++ src/state/Areas/AreaTreeLevels/selectors.js
@@ -6,8 +6,9 @@ const getAll = common.getAll(getSubstate);
 const getAllAsObject = common.getAllAsObject(getSubstate);
 const getActiveKey = common.getActiveKey(getSubstate);
 const getActive = common.getActive(getSubstate);
-
+const getIndexed = common.getIndexed(getSubstate);
 export default {
+	getIndexed,
 	getAll,
 	getAllAsObject,
 	getActiveKey,
